a38826dc2c42720f409c365cedb6ac3d7fecc314
Sven Jacobs <mail@svenjacobs.com>
2012-01-20 20:51:16 +0100

    Working on build tool

---
+++ DEVELOPMENT.md
@@ -0,0 +1,10 @@
+Development guidelines
+======================
+
+Still in the works...
+
+JavaScript
+----------
+
+* According to Google's [JavaScript Style Guide](http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml).
+* Files are validated with JSLint and Google Clojure Compiler. There should be no errors or warnings.
--- build.js
+++ build.js
@@ -1,10 +1,98 @@
 #!/usr/bin/env node
-// node.js script that will torture all files from /js through a JSLint verification.
+/*jslint white: true, plusplus: true, nomen: true, indent: 4*/
+/*global require, console*/
 
-(function () {
+/**
+ * node.js script that will torture all files from /js through a JSLint verification.
+ */
+(function() {
     "use strict";
 
-    var fs = require("fs"),
+    function Build() {
+        this.fs = require("fs");
+        this.path = require("path");
+        //this._s = require("underscore.string");
+    }
+
+    /**
+     * Runs the build process.
+     *
+     * @param path The source path
+     * @param output The output/destination path
+     */
+    Build.prototype.run = function(path, output) {
+        this.errors = [];
+        this.dir(path, output);
+    };
+
+    /**
+     * Iterates over directory structure.
+     *
+     * @private
+     */
+    Build.prototype.dir = function(path, output) {
+        this.fs.readdir(path, this.e(function(files) {
+            files.forEach(function(file) {
+                // Ignore files/directories starting with a .
+                if (file[0] === ".") {
+                    return;
+                }
+
+                var p = this.path.join(path, file);
+
+                this.fs.stat(p, this.e(function(stats) {
+                    if (stats.isDirectory()) {
+                        this.dir(p, output);
+                    } else if (stats.isFile()) {
+                        this.file(p, output);
+                    }
+                }));
+            }, this);
+        }));
+    };
+
+    /**
+     * @private
+     */
+    Build.prototype.file = function(file, output) {
+        // TODO
+    };
+
+    /**
+     * Creates a generic callback function that handles errors.
+     *
+     * Returned function assumes that first argument is the error object
+     * as is the case with fs.readdir() for example.
+     *
+     * @private
+     */
+    Build.prototype.e = function(callback) {
+        var self = this;
+        return function() {
+            var args = [],
+                err = arguments['0'],
+                i;
+
+            if (err) {
+                console.error(err);
+                this.errors.push(err);
+            } else {
+                // arguments is an object not an array so we need to convert it
+                for (i = 1; i < arguments.length; i++) {
+                    args.push(arguments[i]);
+                }
+
+                callback.apply(self, args);
+            }
+        };
+    };
+
+    var build = new Build();
+
+    build.run(".", ".");
+
+
+    /*var fs = require("fs"),
         lint = require("./build/jslint.js"),
         DIR = "src/js";
 
@@ -37,6 +125,6 @@
                 console.log("All OK. You rock!");
             }
         }
-    });
+    });*/
 }());
 
